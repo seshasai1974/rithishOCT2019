a84d2aa98364cf576627362e8bf8a2016528a7da
rithish <rithish@10.0.0.70>
2019-10-02 13:21:15 -0400

    new variable added xyz

--- oops-concepts/src/main/java/com/java/ood/concepts/composition/Composition.java
+++ oops-concepts/src/main/java/com/java/ood/concepts/composition/Composition.java
@@ -62,6 +62,7 @@ class Product {
 	private int id;
 	private String name;
 	private String description;
+	private String xyz;
 
 	public Product(int id, String name, String description) {
 		super();
